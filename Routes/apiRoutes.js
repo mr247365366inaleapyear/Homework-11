cb6ebee93040b38621cac713f41b42ca65da2f55
Waleed McLean <mr247365366inaleapyear@gmail.com>
2021-02-20 01:47:02 -0500

    formatted files with prettier

--- Routes/apiRoutes.js
+++ Routes/apiRoutes.js
@@ -7,30 +7,34 @@ const db = require("../Develop/db/db.json");
 const notes = [];
 
 module.exports = (app) => {
+  app.get("/api/notes", (request, result) => {
+    fs.readFile(
+      path.join(__dirname, "../Develop/db/db.json"),
+      "utf8",
+      (error, data) => {
+        res.json(data);
+      }
+    );
+  });
 
-    app.get("/api/notes", (request, result) => {
-        fs.readFile(path.join(__dirname, "../Develop/db/db.json"), "utf8", (error, data) => {
-            res.json(data)
-        });
-    });
-
-    app.post("/api/notes", (req, res) => {
-        fs.readFile(path.join(__dirname, "../Develop/db/db.json"), "utf8", (error, data) => {
-            res.json(data)
-        });
-
+  app.post("/api/notes", (req, res) => {
+    fs.readFile(
+      path.join(__dirname, "../Develop/db/db.json"),
+      "utf8",
+      (error, data) => {
+        res.json(data);
+      }
+    );
 
     app.delete("/api/notes/:id", (req, res) => {
-        fs.readFile(path.join(__dirname, "../Develop/db/db.json"), "utf8", (error, data) => {
-            
-            notes = JSON.parse(data);
-            console.log(notes);
-        });
-   
-   
-   
-    }) 
-
-        
-
-})};
+      fs.readFile(
+        path.join(__dirname, "../Develop/db/db.json"),
+        "utf8",
+        (error, data) => {
+          notes = JSON.parse(data);
+          console.log(notes);
+        }
+      );
+    });
+  });
+};
