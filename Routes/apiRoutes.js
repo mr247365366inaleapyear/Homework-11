6626e1a4419f655d580267fbc8adc7b82e7172ab
Waleed McLean <mr247365366inaleapyear@gmail.com>
2021-02-20 01:45:08 -0500

    editing apiRoutes.js

--- Routes/apiRoutes.js
+++ Routes/apiRoutes.js
@@ -8,13 +8,13 @@ const notes = [];
 
 module.exports = (app) => {
 
-    app.get("/api/notes", (req, res) => {
+    app.get("/api/notes", (request, result) => {
         fs.readFile(path.join(__dirname, "../Develop/db/db.json"), "utf8", (error, data) => {
             res.json(data)
         });
     });
 
-    app.get("/api/notes", (req, res) => {
+    app.post("/api/notes", (req, res) => {
         fs.readFile(path.join(__dirname, "../Develop/db/db.json"), "utf8", (error, data) => {
             res.json(data)
         });
@@ -22,14 +22,11 @@ module.exports = (app) => {
 
     app.delete("/api/notes/:id", (req, res) => {
         fs.readFile(path.join(__dirname, "../Develop/db/db.json"), "utf8", (error, data) => {
-            //check syntax of notes and console log
             
             notes = JSON.parse(data);
             console.log(notes);
         });
    
-        //filter out noteId that I am looking for
-        //write the new notes that don't have the noteId that does not have the file
    
    
     }) 
